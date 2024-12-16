8eeb450f50155d9a2a6cbd728ffad2cd72392683
Pankaj Sharma <pankaj.avology@gmail.com>
2024-12-16 18:29:30 +0530

    Updated and create components

--- assets/js/main.js
+++ assets/js/main.js
@@ -122,18 +122,19 @@ $(document).ready(function() {
       autoplay: true,
       autoplaySpeed: 1000,
       responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+              slidesToShow: 2
+          }
+      },
           {
               breakpoint: 768,
-              settings: {
-                  slidesToShow: 2
-              }
-          },
-          {
-              breakpoint: 480,
               settings: {
                   slidesToShow: 1
               }
           }
+         
       ]
   });
 });
